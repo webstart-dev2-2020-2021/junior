e411672b3b26da925e106e7a6c1b23e67f011592
Junior <duartejusanca@gmail.com>
2020-11-23 11:33:36 +0100

    ajout des dossiers: config et models ainsi que le fichier database.sqlite3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 const urlencoderParser = bodyParser.urlencoded({ extended: false })
 
+const User = require('./models')
+
 const app = express()
 app.use(helmet())
 app.use(express.static('public'))
@@ -14,6 +16,23 @@ const port = 3000
 
 app.get('/', (req, res)=>{
     res.render('index.pug')
+})
+
+
+//Récuperer tous les utilisateurs
+app.get('/admin', async (req, res)=>{
+    //promesse
+    //
+    //
+    try{
+        const users = await User.findAll()
+        console.log('users->', users)
+        res.render('admin.pug')
+    }catch (error) {
+        console.log('error dans le user findAll()',error)
+        //renvoyer error 500
+        //res.render
+    }
 })
 
 app.get('/singup', (req, res) =>{
